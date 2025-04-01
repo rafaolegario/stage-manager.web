ecd6c37fba62b6a99f4daa791a315c49fdd548a3
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-01 17:40:39 -0300

    feat: remove search build

--- src/dashboard/builds/home-build.ts
+++ src/dashboard/builds/home-build.ts
@@ -1,115 +1,111 @@
-import { VerifyStatus, StatusData } from "../../utils/VerifyStatus";
-import { GetAllInterns } from "../../http/get-all-interns";
-import { Intern } from "../../@types/intern";
+import { VerifyStatus, StatusData } from '../../utils/VerifyStatus'
+import { Intern } from '../../@types/intern'
 
-export async function buildHome(): Promise<void> {
+export async function buildHome(
+  data: Intern[],
+  search?: boolean,
+): Promise<void> {
+  search = search ?? false
 
-  const data: Intern[] = await GetAllInterns();
+  const InfoDiv = document.createElement('div')
 
-  const Dashboard: HTMLElement | null = document.querySelector('.dashboard');
+  InfoDiv.className = 'info'
+
+  document.querySelector('.dashboard')?.append(InfoDiv)
+
+  const Dashboard: HTMLElement | null = document.querySelector('.info')
+
   if (!Dashboard) {
-    console.error("Elemento .dashboard não encontrado!");
-    return;
+    return
   }
-  Dashboard.innerHTML = ''; 
 
-  const homeDiv: HTMLDivElement = document.createElement("div");
-  homeDiv.className = "home";
+  Dashboard.innerHTML = ''
 
-  const header: HTMLElement = document.createElement("header");
-  const headerTitle: HTMLHeadingElement = document.createElement("h2");
-  headerTitle.textContent = "/Home";
-  header.appendChild(headerTitle);
+  const homeDiv: HTMLDivElement = document.createElement('div')
+  homeDiv.className = 'home'
 
-  const searchDiv: HTMLDivElement = document.createElement("div");
-  searchDiv.className = "search";
+  const internsDiv: HTMLDivElement = document.createElement('div')
+  internsDiv.className = 'interns'
 
-  const searchIcon: HTMLElement = document.createElement("i");
-  searchIcon.className = "fa-solid fa-magnifying-glass";
-  searchDiv.appendChild(searchIcon);
-
-  const searchInput: HTMLInputElement = document.createElement("input");
-  searchInput.type = "text";
-  searchInput.placeholder = "Pesquisar estagiário...";
-  searchDiv.appendChild(searchInput);
-
-  header.appendChild(searchDiv);
-  homeDiv.appendChild(header);
-
-  const internsDiv: HTMLDivElement = document.createElement("div");
-  internsDiv.className = "interns";
-
-  
-  if (!data || data.length === 0) {
+  if (!data || (data.length === 0 && search)) {
     internsDiv.style.color = '#fff'
-    internsDiv.innerText = "Nenhum estagiário cadastrado!";
+    const p = document.createElement('p')
+    const i = document.createElement('i')
+    p.innerText = 'Nenhum estagiário encontrado!'
+    i.className = 'fa-solid fa-file-excel'
+    i.style.fontSize = '45px'
+    internsDiv.append(p, i)
+  }
+
+  if (!data || (data.length === 0 && !search)) {
+    internsDiv.style.color = '#fff'
+    internsDiv.innerText = 'Nenhum estagiário cadastrado!'
   } else {
-  
     data.forEach((intern: Intern) => {
-      const internDiv: HTMLDivElement = document.createElement("div");
-      internDiv.className = "intern";
+      const internDiv: HTMLDivElement = document.createElement('div')
+      internDiv.className = 'intern'
 
-      const userDiv: HTMLDivElement = document.createElement("div");
-      userDiv.className = "user";
+      const userDiv: HTMLDivElement = document.createElement('div')
+      userDiv.className = 'user'
 
-      const userIcon: HTMLElement = document.createElement("i");
-      userIcon.className = "fa-solid fa-user";
-      userDiv.appendChild(userIcon);
+      const userIcon: HTMLElement = document.createElement('i')
+      userIcon.className = 'fa-solid fa-user'
+      userDiv.appendChild(userIcon)
 
-      const userName: HTMLHeadingElement = document.createElement("h3");
-      userName.textContent = intern.name;
-      userDiv.appendChild(userName);
+      const userName: HTMLHeadingElement = document.createElement('h3')
+      userName.textContent = intern.name
+      userDiv.appendChild(userName)
 
-      internDiv.appendChild(userDiv);
+      internDiv.appendChild(userDiv)
 
-      const infosDiv1: HTMLDivElement = document.createElement("div");
-      infosDiv1.className = "infos";
+      const infosDiv1: HTMLDivElement = document.createElement('div')
+      infosDiv1.className = 'infos'
 
-      const functionLabel: HTMLParagraphElement = document.createElement("p");
-      functionLabel.textContent = "Função:";
-      infosDiv1.appendChild(functionLabel);
+      const functionLabel: HTMLParagraphElement = document.createElement('p')
+      functionLabel.textContent = 'Função:'
+      infosDiv1.appendChild(functionLabel)
 
-      const functionValue: HTMLSpanElement = document.createElement("span");
-      functionValue.textContent = intern.role;
-      infosDiv1.appendChild(functionValue);
+      const functionValue: HTMLSpanElement = document.createElement('span')
+      functionValue.textContent = intern.role
+      infosDiv1.appendChild(functionValue)
 
-      internDiv.appendChild(infosDiv1);
+      internDiv.appendChild(infosDiv1)
 
-      const infosDiv2: HTMLDivElement = document.createElement("div");
-      infosDiv2.className = "infos";
+      const infosDiv2: HTMLDivElement = document.createElement('div')
+      infosDiv2.className = 'infos'
 
-      const statusLabel: HTMLParagraphElement = document.createElement("p");
-      statusLabel.textContent = "Status:";
-      infosDiv2.appendChild(statusLabel);
+      const statusLabel: HTMLParagraphElement = document.createElement('p')
+      statusLabel.textContent = 'Status:'
+      infosDiv2.appendChild(statusLabel)
 
       const statusData: StatusData = VerifyStatus({
-        status: intern.on_work,
-        getIn: intern.getIn_hour,
-        getOut: intern.getOut_hour
-      });
+        status: intern.onWork,
+        getIn: intern.getInHour,
+        getOut: intern.getOutHour,
+      })
 
-      const statusIcon: HTMLElement = document.createElement("i");
-      statusIcon.className = statusData.icon;
-      infosDiv2.appendChild(statusIcon);
+      const statusIcon: HTMLElement = document.createElement('i')
+      statusIcon.className = statusData.icon
+      infosDiv2.appendChild(statusIcon)
 
-      const statusValue: HTMLSpanElement = document.createElement("span");
-      statusValue.textContent = ` ${statusData.situation}`;
-      infosDiv2.appendChild(statusValue);
+      const statusValue: HTMLSpanElement = document.createElement('span')
+      statusValue.textContent = ` ${statusData.situation}`
+      infosDiv2.appendChild(statusValue)
 
-      internDiv.appendChild(infosDiv2);
+      internDiv.appendChild(infosDiv2)
 
-      const eyeIcon: HTMLElement = document.createElement("i");
-      eyeIcon.className = "fa-solid fa-eye";
+      const eyeIcon: HTMLElement = document.createElement('i')
+      eyeIcon.className = 'fa-solid fa-eye'
       // eyeIcon.addEventListener('click', ()=>{
       //   InternProfile(intern.intern_id)
       // })
-      internDiv.appendChild(eyeIcon);
+      internDiv.appendChild(eyeIcon)
 
-      internsDiv.appendChild(internDiv);
-    });
+      internsDiv.appendChild(internDiv)
+    })
   }
 
-  homeDiv.appendChild(internsDiv);
+  homeDiv.appendChild(internsDiv)
 
-  Dashboard.appendChild(homeDiv);
+  Dashboard.appendChild(homeDiv)
 }
