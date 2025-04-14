efd7dbb7c284c6e94b9238abe9051d9ed5c2e0a2
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-14 16:58:14 -0300

    feat: get activities http

---
+++ src/http/get-intern-activites.ts
@@ -0,0 +1,9 @@
+import { activity } from "../@types/activity"
+
+
+
+export async function GetInternActivities(id:string) {
+  // conexão HTTP
+  const activities:activity[] = []
+  return activities
+}
