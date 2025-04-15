f8a82d802018ec72d36228df04194719067247db
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-15 13:27:36 -0300

    feat: add address

--- src/http/create-intern.ts
+++ src/http/create-intern.ts
@@ -1,13 +1,22 @@
 import { InternDataInput } from '../@types/intern'
-import { interns } from '../FakeDatabase'
+import { Address, interns } from '../FakeDatabase'
 
 export async function CreateIntern(data: InternDataInput) {
-  
+  const id = (Math.random() * 200).toString()
   interns.push({
-    id: (Math.random() * 200).toString(),
+    id,
     ...data,
     onWork: false,
     delayed: 0,
     absent: 0,
   })
+
+  Address.push({
+    id: (Math.random() * 660).toString(),
+    city: data.city,
+    cep: data.cep,
+    street: data.street,
+    neighborhood: data.neighborhood,
+    houseNumber: data.houseNumber,
+    internId: [id],})
 }
