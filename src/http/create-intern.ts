104fdd3e35c69619df5b116844805e0df36c34d1
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-15 11:31:18 -0300

    feat: rename id

--- src/http/create-intern.ts
+++ src/http/create-intern.ts
@@ -4,7 +4,7 @@ import { interns } from '../FakeDatabase'
 export async function CreateIntern(data: InternDataInput) {
   
   interns.push({
-    internId: (Math.random() * 200).toString(),
+    id: (Math.random() * 200).toString(),
     ...data,
     onWork: false,
     delayed: 0,
