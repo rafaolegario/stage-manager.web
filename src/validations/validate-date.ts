7a5d93798e388abeca33e5c329fcc447441bfc3d
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-15 11:28:57 -0300

    fix: fix day

--- src/validations/validate-date.ts
+++ src/validations/validate-date.ts
@@ -7,9 +7,10 @@ interface ValidateDateParms {
 
 export function ValidateDate({ startDate, endDate }: ValidateDateParms) {
   const start = dayjs(startDate)
+  const dayBefore = start.subtract(1, 'day');
   const end = dayjs(endDate)
 
-  if (end.isBefore(start)) {
+  if (end.isBefore(dayBefore)) {
     throw new Error('A data de conclusão deve ser maior que a data ínicio')
   }
 }
