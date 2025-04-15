e76802097f6c44732d1caecc2b542e0db4160f27
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-04-15 11:32:37 -0300

    feat: create controller to get activity fields

---
+++ src/dashboard/controllers/get-form-data-to-create-activity.ts
@@ -0,0 +1,38 @@
+import { ZodError } from 'zod'
+import { sendFieldsErrorNotification } from '../../utils/send-fields-error-notification'
+import { ToastfyPopUp } from '../../utils/toastfy-popup'
+import { ValidateDate } from '../../validations/validate-date'
+import { activityInput } from '../../@types/activity'
+import { ValidateActivityFields } from '../../validations/validate-activity-fields'
+import { CreateActivity } from '../../http/create-activity'
+
+export async function GetFormDataToCreateActivity() {
+  try {
+    const form: HTMLFormElement | null =
+      document.querySelector('#create-form')
+
+    if (!form) {
+      return
+    }
+
+    const formData = new FormData(form)
+
+    const date = new Date().toISOString()
+    const data: activityInput = ValidateActivityFields(formData)
+    ValidateDate({ startDate: date, endDate: data.dueDate.toISOString() })
+
+    await CreateActivity(data)
+    ToastfyPopUp('Atividade criada com sucesso!', 'green')
+    form.reset()
+  } catch (error) {
+
+    if (error instanceof ZodError) {
+      sendFieldsErrorNotification(error);
+    }
+
+    else if(error instanceof Error){
+      ToastfyPopUp(error.message,'blue')
+    }
+ 
+  }
+}
