d9730b7d32b10e916b3c2debc85a17d38b242fbb
rafaolegario <rafaelbisofficel@gmail.com>
2025-04-29 22:45:09 -0300

    feat: create phone and cep masks

--- src/utils/masks.ts
+++ src/utils/masks.ts
@@ -9,3 +9,28 @@ export function CpfMask(value: string): string {
 
   return value
 }
+
+export function CepMask(value: string): string {
+  value = value.replace(/[^0-9]/g, '')
+
+  if (value.length <= 5) {
+    return value
+  }
+
+  return value
+    .replace(/^(\d{5})(\d{3})$/, '$1-$2')
+
+}
+
+export function PhoneMask(value: string): string {
+  value = value.replace(/[^0-9]/g, '')
+
+  if (value.length <= 10) {
+    return value
+      .replace(/^(\d{2})(\d{5})(\d{4})$/, '($1)$2-$3')
+  }
+
+  return value
+    .slice(0, 11)
+    .replace(/^(\d{2})(\d{5})(\d{4})$/, '($1)$2-$3')
+}
