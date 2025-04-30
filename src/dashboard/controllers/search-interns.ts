d5306b54908bd90caf11cd9641704f4626ea5d52
rafaolegario <rafaelbisofficel@gmail.com>
2025-04-29 22:44:01 -0300

    feat: implements search logic

--- src/dashboard/controllers/search-interns.ts
+++ src/dashboard/controllers/search-interns.ts
@@ -1,8 +1,17 @@
-import { Intern } from '../../@types/intern'
-import { GetSearchInterns } from '../../http/get-search-interns'
-import { buildHome } from '../builds/home-build'
+import { InternWithAddress } from "../../@types/intern"
+import { GetAllInterns } from "../../http/get-all-interns"
+import { GetSearchInterns } from "../../http/get-search-interns"
+import { buildHome } from "../builds/home-build"
+
 
 export async function SearchIntern(query: string) {
-  const data: Intern[] = await GetSearchInterns(query)
-  buildHome(data, true)
+
+  let data: InternWithAddress[]= []
+  if(query){
+    data = await GetSearchInterns(query)
+    buildHome(data, true)
+  }else{
+    data = await  GetAllInterns()
+    buildHome(data)
+  }
 }
